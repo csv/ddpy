176cae82ee58f99436f947ba1c30831e1e799aeb
Thomas Levine <occurrence@thomaslevine.com>
2013-10-12 15:15:17 +0000

    cases

--- ddpy.py
+++ ddpy.py
@@ -1,8 +1,8 @@
 def to_midi(table, filename):
     '''
     Args:
-        table: A list of dictionaries, a dictionary of lists,
-               or a pandas.DataFrame.
+        table: An iterable of dict-likes, a dict-like of iterables,
+               or a pandas.DataFrame-like.
         filename: A string (like 'output/data.midi')
     Returns:
         None
@@ -12,6 +12,11 @@ def to_midi(table, filename):
     >>> to_midi([[2010,8],[2011,9]], 'gdp.midi')
     ValueError
     '''
+    if _is_like_dataframe(table):
+    elif _is_like_dict(table):
+    elif _is_iterable(table):
+    else:
+        raise ValueError
 
 if __name__ == '__main__':
     import doctest
