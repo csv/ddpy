9b590ca4821a091ca036e8d719d8d6b5859ac297
Thomas Levine <occurrence@thomaslevine.com>
2013-10-12 15:19:02 +0000

    separate the type-checker

--- ddpy.py
+++ ddpy.py
@@ -1,3 +1,5 @@
+import itertools
+
 def to_midi(table, filename):
     '''
     Args:
@@ -12,9 +14,34 @@ def to_midi(table, filename):
     >>> to_midi([[2010,8],[2011,9]], 'gdp.midi')
     ValueError
     '''
+
+
+def _check_types(table):
+    '''
+    Args:
+        table: An iterable of dict-likes, a dict-like of iterables,
+               or a pandas.DataFrame-like.
+    Returns:
+        None
+    Raises:
+        ValueError on invalid input
+
+    >>> _check_types([{'year':2010,'gdp':8},{'year':2011,'gdp':9}])
+
+    >>> _check_types([[2010,8],[2011,9]])
+    ValueError
+    '''
     if _is_like_dataframe(table):
+        pass
     elif _is_like_dict(table):
+        are_iterable = map(_is_iterable, table.values())
+        if not set(types) == {True}:
+            raise ValueError
     elif _is_iterable(table):
+        first = table.next()
+        table = itertools.chain([first], table)
+        if not _is_like_dict(first):
+            raise ValueError
     else:
         raise ValueError
 
