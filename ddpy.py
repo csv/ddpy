eea8e7909d413e3ffb5554f6ad577ecf8d4d3923
Thomas Levine <occurrence@thomaslevine.com>
2013-10-12 15:11:16 +0000

    list of lists

--- ddpy.py
+++ ddpy.py
@@ -2,7 +2,7 @@ def to_midi(table, filename):
     '''
     Args:
         table: A list of dictionaries, a dictionary of lists,
-               a list of lists, or a pandas.DataFrame.
+               or a pandas.DataFrame.
         filename: A string (like 'output/data.midi')
     Returns:
         None
@@ -10,6 +10,6 @@ def to_midi(table, filename):
     >>> to_midi([{'year':2010,'gdp':8},{'year':2011,'gdp':9}], 'gdp.midi')
     None
 
-    >>> to_midi([[2010,8},{'year':2011,'gdp':9}], 'gdp.midi')
+    >>> to_midi([[2010,8],[2011,9]], 'gdp.midi')
     ValueError
     '''
