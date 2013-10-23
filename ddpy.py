13f11ebb2a9cc8e5914771e3954938a758f5509f
Thomas Levine <occurrence@thomaslevine.com>
2013-10-23 10:55:38 -0700

    documentation

--- ddpy.py
+++ ddpy.py
@@ -1,5 +1,33 @@
 import itertools
 
+from midiutil.MidiFile import MIDIFile
+
+# Create the MIDIFile Object with 1 track
+MyMIDI = MIDIFile(1)
+
+# Tracks are numbered from zero. Times are measured in beats.
+track = 0
+time = 0
+
+# Add track name and tempo.
+MyMIDI.addTrackName(track,time,"Sample Track")
+MyMIDI.addTempo(track,time,120)
+
+# Add a note. addNote expects the following information:
+track = 0
+channel = 0
+pitch = 60
+time = 0
+duration = 1
+volume = 100
+
+MyMIDI.addNote(track,channel,pitch,time,duration,volume)
+
+# And write it to disk.
+binfile = open("output.mid", 'wb')
+MyMIDI.writeFile(binfile)
+binfile.close()
+
 def to_midi(table, filename, params):
     '''
     Args:
--- readme.md
+++ readme.md
@@ -9,3 +9,17 @@ and a `from_midi` function.
 
 We plan on adding `to_midi` and `from_midi` as `pandas.DataFrame`
 methods eventually.
+
+## Table structure
+In the present documentation, we use "table" in a rather generic way.
+It refers to a collection of things (rows), with the same attributes
+(columns) measured about each thing. `ddpy` recognizes two data
+structures for representating a table.
+
+1. A list of dicts
+2. Pandas DataFrame
+
+Each column in the table gets represented as an instrument, and each
+row in the table gets represented as a beat.
+
+## Column translations
