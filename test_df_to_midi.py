973e6e4a6a6fe6ae32c6c60e2c5e021151fafa8d
Thomas Levine <occurrence@thomaslevine.com>
2013-11-02 04:26:45 -0700

    text

--- test_df_to_midi.py
+++ test_df_to_midi.py
@@ -14,13 +14,32 @@ def test_one_int_column():
     for time,note in enumerate([38, 40, 42, 43]):
         expected.addNote(0,0,note,time,1,100)
 
-    expected.writeFile(open("output.mid", 'wb'))
-
     df = pandas.DataFrame([
         {'guitar':38},
         {'guitar':40},
         {'guitar':42},
         {'guitar':43},
+    ])
+    observed = df_to_midi(df, bpm = 120)
+
+    n.assert_equal(observed, expected)
+
+def test_one_text_column():
+    '''
+    A data frame with a single integer column
+    should be converted correctly.
+    '''
+    expected = MIDIFile(1)
+    expected.addTrackName(0,0,"vocals")
+    expected.addTempo(0,0,120)
+    for time,note in enumerate([38, 40, 42, 43]):
+        expected.addNote(0,0,note,time,1,100)
+
+    df = pandas.DataFrame([
+        {'vocals':38},
+        {'vocals':40},
+        {'vocals':42},
+        {'vocals':43},
     ])
     observed = df_to_midi(df, bpm = 120)
 
