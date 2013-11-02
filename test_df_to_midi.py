b0e7088254715e8d0167660e4ace076a5b439728
Thomas Levine <occurrence@thomaslevine.com>
2013-11-02 04:13:16 -0700

    ordering

--- test_df_to_midi.py
+++ test_df_to_midi.py
@@ -3,6 +3,18 @@ import pandas
 from ddpy import df_to_midi
 
 def test_one_int_column():
+    '''
+    A data frame with a single integer column
+    should be converted correctly.
+    '''
+    expected = MIDIFile(1)
+    expected.addTrackName(0,0,"guitar")
+    expected.addTempo(0,0,120)
+    for time,note in enumerate([38, 40, 42, 43]):
+        expected.addNote(0,0,note,time,1,100)
+
+    expected.writeFile(open("output.mid", 'wb'))
+
     df = pandas.DataFrame([
         {'guitar':38},
         {'guitar':40},
@@ -11,10 +23,9 @@ def test_one_int_column():
     ])
     observed = df_to_midi(df, bpm = 120)
 
-    expected = MIDIFile(1)
-    expected.addTrackName(0,0,"guitar")
-    expected.addTempo(0,0,120)
-    for time,note in enumerate([38, 40, 42, 43]):
-        expected.addNote(0,0,note,time,1,100)
-
     n.assert_equal(observed, expected)
+
+
+
+
+
