3000b3bd89567ab195ce37a6afea9e0037c0504b
Thomas Levine <occurrence@thomaslevine.com>
2013-11-07 05:03:07 -0800

    check for missing value

--- test_df_to_midi.py
+++ test_df_to_midi.py
@@ -1,6 +1,8 @@
 import nose.tools as n
 import pandas
+import numpy
 from midiutil.MidiFile import MIDIFile
+
 from ddpy import df_to_midi
 
 def assert_midi_equal(a, b):
@@ -23,6 +25,26 @@ def test_one_int_column():
     df = pandas.DataFrame([
         {'guitar':38},
         {'guitar':40},
+        {'guitar':42},
+        {'guitar':43},
+    ])
+    observed = df_to_midi(df, bpm = 120)
+    assert_midi_equal(observed, expected)
+
+def test_missing_value():
+    '''
+    A data frame with a single integer column
+    should be converted correctly.
+    '''
+    expected = MIDIFile(1)
+    expected.addTrackName(0,0,"guitar")
+    expected.addTempo(0,0,120)
+    for time,note in enumerate([38, 42, 43]):
+        expected.addNote(0,0,note,time,1,100)
+
+    df = pandas.DataFrame([
+        {'guitar':38},
+        {'guitar':numpy.nan},
         {'guitar':42},
         {'guitar':43},
     ])
